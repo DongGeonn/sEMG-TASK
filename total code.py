079ccca2f13896078c51cb7ad3f1e71adba5d169
DESKTOP-J88PELM\user <kdg0366@naver.com>
2023-04-09 01:53:00 +0900

    data_first 딕셔너리 생성
    label과 rep을 key name으로 가지는 딕셔너리 생성하고 mat파일값 분류

--- total code.py
+++ total code.py
@@ -8,3 +8,34 @@ mat_file=scipy.io.loadmat(mat_file_name)
 emg=mat_file['emg']
 label=mat_file['label']
 rep=mat_file['repetition']
+
+# data_first 빈 딕셔너리 만들기
+data_first={}
+for num_label in range(1,18):                                                          
+    for num_rep in range(1,7):
+        key_name="L"+str(num_label)+"-"+str(num_rep)
+        data_first[key_name]=[]
+
+
+# data 딕셔너리의 value에 입력값을 추가하는 함수 설정
+def dicplus(dictionary, key, value):                                            
+    value_list=dictionary[key]
+    value_list.append(value)
+    dictionary[key]=value_list
+
+
+# data_first 딕셔너리의 각 key에 value 입력
+for i in range(emg.shape[0]):                                                  
+    for num_label in range(1,18):
+        for num_rep in range(1,7):
+            if num_label==label[i] and num_rep==rep[i]:
+                key_name="L"+str(num_label)+"-"+str(num_rep)       
+                dicplus(data_first, key_name, emg[i,:])
+
+
+# data_first 딕셔너리의 각 valve를 numpy형태로 설정                
+import numpy as np                  
+for num_label in range(1,18):                                                   
+    for num_rep in range(1,7):
+        key_name="L"+str(num_label)+"-"+str(num_rep)
+        data_first[key_name]=np.array(data_first[key_name])
